748494bede88922535bc360591dea40949047a38
Annahlu <analuizamartinscesario@gmail.com>
2020-11-17 13:32:58 -0300

    fazendo menu lateral do mobile - aparece ao clicar no botao da direita

---
+++ assets/js/menu-modal.js
@@ -0,0 +1,23 @@
+// Conteúdo que deseja inserir
+var oDiv = '<div class="modal">  <p> aaaaaaa <p>  </div>';
+
+
+// Captura o evento de clique
+$('a[href="#menu-mobile-ativado"]').click(function(event) {
+
+    // Previne que o link recarregue a página
+	event.preventDefault();
+
+    // Checa se a class modal existe
+	if(!$('.modal').length) {
+		
+		// Se não existir, insere
+		$(this).after(oDiv);
+	}
+	else {
+		
+		// Se existir da um fadeToggle(); // trocar fade toggle por slide toggle
+		$('.modal').fadeToggle();
+	}
+	
+});
