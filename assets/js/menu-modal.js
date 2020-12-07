c682024244fe46314b6c6f4c93922f0dd57dffc1
Annahlu <analuizamartinscesario@gmail.com>
2020-12-07 20:11:55 -0300

    atualização menu-mobile

--- assets/js/menu-modal.js
+++ assets/js/menu-modal.js
@@ -1,7 +1,13 @@
 // Conteúdo que deseja inserir
 
-var oDiv = ' <div class="modal menu-lateral-modal"> <p class="p-menu-modal">  <a class="menu-desktop" href="compartilhar.html"><div class="menu- desktop elem-menu-lateral"><img  class="menu-desktop" src="assets/img/compartilhar.svg"><p class="menu-desktop"> Compartilhar <p> <div> </div>';           
-
+var oDiv = ' <div class="modal menu-lateral-modal"> <ul class="ULmodal"> <li> <div class="espaco-3"></div>  <img class ="logo-eic-modal-centro" src="assets/img/logo-eic-branco.png">  </li> <li> <br> </li>  <li> <div><a href="#"> <img src="assets/img/home.svg"> <p> Início </p> </a> </li> <li> <p> Sobre o jogo </p> </li> </ul>  </div>';           
+/*<ul class="menu-desktop tipoUL">
+        <li class = "espaco-6 menu-desktop"></li>
+        <li> <!--Colocar o logo no centro-->
+            <a class="menu-desktop" href="index.html"> 
+                <img class="logo-eic-centro menu-desktop" src="assets/img/logo-eic-branco.png">
+            </a>
+        </li>*/ 
 // Captura o evento de clique
 $('a[href="#menu-mobile-ativado"]').click(function(event) {
 
