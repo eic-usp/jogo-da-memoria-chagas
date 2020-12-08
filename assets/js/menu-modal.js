7e50d9bf8de9e46ce4e2f7a8ed8f029b2562752e
Annahlu <analuizamartinscesario@gmail.com>
2020-12-08 11:48:39 -0300

    progesso - problema no menu modal

--- assets/js/menu-modal.js
+++ assets/js/menu-modal.js
@@ -1,6 +1,6 @@
 // Conteúdo que deseja inserir
 
-var oDiv = ' <div class="modal menu-lateral-modal"> <ul class="ULmodal"> <li> <div class="espaco-3"></div>  <img class ="logo-eic-modal-centro" src="assets/img/logo-eic-branco.png">  </li> <li> <br> </li>  <li> <div><a href="#"> <img src="assets/img/home.svg"> <p> Início </p> </a> </li> <li> <p> Sobre o jogo </p> </li> </ul>  </div>';           
+var oDiv = ' <div class="modal menu-lateral-modal"> <ul class="ULmodal"> <li class="espaco-3"> </li> <li> <img class ="logo-eic-modal-centro" src="assets/img/logo-eic-branco.png">  </li> <li> <br> </li> <li> <br> </li> <li "largura-p-modal"> <a href="#"> <div style="width: 200px;">  <img  class="elem-menu-modal"  src="assets/img/home.svg"> <p> Início </p> </div> </a> </li> <li> <p> Sobre o jogo </p> </li>  <li> Créditos </li> <li> Compartilhar  </li></ul> </div>';           
 /*<ul class="menu-desktop tipoUL">
         <li class = "espaco-6 menu-desktop"></li>
         <li> <!--Colocar o logo no centro-->
