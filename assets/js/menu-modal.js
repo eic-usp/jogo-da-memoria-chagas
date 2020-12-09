62087fbd853fa69b402dea5deb84b34604bec874
Annahlu <analuizamartinscesario@gmail.com>
2020-12-09 12:24:41 -0300

    Progresso do dia

--- assets/js/menu-modal.js
+++ assets/js/menu-modal.js
@@ -1,13 +1,13 @@
 // Conteúdo que deseja inserir
 
-var oDiv = ' <div class="modal menu-lateral-modal"> <ul class="ULmodal"> <li class="espaco-3"> </li> <li> <img class ="logo-eic-modal-centro" src="assets/img/logo-eic-branco.png">  </li> <li> <br> </li> <li> <br> </li> <li "largura-p-modal"> <a href="#"> <div style="width: 200px;">  <img  class="elem-menu-modal"  src="assets/img/home.svg"> <p> Início </p> </div> </a> </li> <li> <p> Sobre o jogo </p> </li>  <li> Créditos </li> <li> Compartilhar  </li></ul> </div>';           
-/*<ul class="menu-desktop tipoUL">
-        <li class = "espaco-6 menu-desktop"></li>
-        <li> <!--Colocar o logo no centro-->
-            <a class="menu-desktop" href="index.html"> 
-                <img class="logo-eic-centro menu-desktop" src="assets/img/logo-eic-branco.png">
-            </a>
-        </li>*/ 
+var oDiv = ' <div class="modal menu-lateral-modal"> <ul class="ULmodal"> <li class="espaco-3"> </li> <li> <img class ="logo-eic-modal-centro" src="assets/img/logo-eic-branco.png">  </li> <li> <br> </li> <li> <br> </li> <li> <a href="#"> <div class="elem-menu-modal">  <img src="assets/img/home.svg"> <p> Início </p> </div> </a> </li> <li> <br> <br> <p> Sobre o jogo </p> </li>  <li> Créditos </li> <li> Compartilhar </li></ul> </div>';           
+//Não to conseguindo colocar a imagem e o texto na mesma linha
+/*<a class="menu-desktop" href="index.html">
+            <div class="menu-desktop elem-menu-lateral">
+            <img class="menu-desktop" src="assets/img/creditos.svg">
+            <p class="menu-desktop"> Créditos <p> 
+            <div>
+            </a>*/
 // Captura o evento de clique
 $('a[href="#menu-mobile-ativado"]').click(function(event) {
 
