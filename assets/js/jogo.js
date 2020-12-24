aec596dc316712ccdbf7d98c22c481614f68411c
Annahlu <analuizamartinscesario@gmail.com>
2020-12-24 13:04:37 -0300

    virar as cartas e fazer o match, precisa mostrar a carta que está atras

--- assets/js/jogo.js
+++ assets/js/jogo.js
@@ -15,8 +15,13 @@
 startGame();
 
 function startGame(){
+    matches = 0;
+
+    flippedCards = [];
+
     var frontFaces = document.getElementsByClassName("front");
     var backFaces = document.getElementsByClassName("back");
+
     
     var size = windowWidth = $(window).width();
     
@@ -25,8 +30,9 @@ function startGame(){
     /*tentar criar uma função e modularizar essa parte passando o tamanho da tela por parâmetro, ver se vai funcionar*/
     
     for(var i = 0; i < 16; i++){
-        /*frontFaces[i].classList.remove("flipped", "match");
-        backFaces[i].classList.remove("flipped", "match");*/
+
+        frontFaces[i].classList.remove("flipped", "match");
+        backFaces[i].classList.remove("flipped", "match");
 
        /*Pode usar get element by id, porem o query selector é mais rápido*/
        var card = document.querySelector("#card" + i);
@@ -47,25 +53,75 @@ function startGame(){
         card.style.top = 410 + "px";
        }
 
-      /* card.addEventListener("click", flipCard, false);*/
+      card.addEventListener("click", flipCard, false);
 
         /*todos os cards tem imagem e id */
         frontFaces[i].style.background = "url('../img/fase1"+images[i].src +"')";
         frontFaces[i].setAttribute("id",images[i].id);
         console.log(frontFaces[i].id);
    }
-
-    
+  
     }else if (size >= 600 && size <= 999) { /*Versão tablet*/
 
     }else{ /*Versão Mobile*/
 
     }
+}
 
+function flipCard(){
+    if(flippedCards.length < 2){
+         /*rotacionar as faces 180 graus*/
+        /*getElementsByClassName retorna uma lista */
+        var faces =  this.getElementsByClassName("face");
 
+        //impedir que o usuario tente clicar duas vezes na mesma carta, e eu nao quero isso
+        if(faces[0].classList.length > 2){
+            return; 
+        }
+        faces[0].classList.toggle("flipped");
+        faces[1].classList.toggle("flipped");
+        /*toggle = switch, se existe adiciona, se ja existe, remove  */
+        flippedCards.push(this);
 
+        if(flippedCards.length === 2 ){
+            //elemento de face front 
+            if(flippedCards[0].childNodes[3].id === flippedCards[1].childNodes[3].id){
+                flippedCards[0].childNodes[1].classList.toggle("match");
+                flippedCards[0].childNodes[3].classList.toggle("match");
+                flippedCards[1].childNodes[1].classList.toggle("match");
+                flippedCards[1].childNodes[3].classList.toggle("match");
 
+                matchCardsSign();
 
+                matches++;
+                flippedCards = [];
+
+                if(matches === 8){
+                    gameOver();
+                }
+            }
+        }
+    } else {
+
+        /*Não entendi e acho que nao vai funcionar */
+       // console.log(flippedCards);
+       flippedCards[0].childNodes[1].classList.toggle("flipped");
+       flippedCards[0].childNodes[3].classList.toggle("flipped");
+       flippedCards[1].childNodes[1].classList.toggle("flipped");
+       flippedCards[1].childNodes[3].classList.toggle("flipped");
+
+       flippedCards = [];
+    }  
+}
+function matchCardsSign(){
+    imgMatchSing.style.zIndex = 1;
+    imgMatchSing.style.top = 150 + "px";
+    imgMatchSing.style.opacity = 0;
+    setTimeout(function(){
+        imgMatchSing.style.zIndex = -1;
+        imgMatchSing.style.top = 250 + "px";
+        imgMatchSing.style.opacity = 1;
+    },1500);
 }
 
  }());
