369f133c2ddfc712a002ec539439d2528fa261e5
Annahlu <analuizamartinscesario@gmail.com>
2020-12-29 17:15:01 -0300

    divider da pagina de compartilhamento feito

--- assets/js/menuLateral.js
+++ assets/js/menuLateral.js
@@ -1,13 +1,13 @@
 // $ para variaveis que guardam refenrecias
-const $divMenuLateral = document.getElementById("aaaa");
+const $divMenuLateral = document.getElementById("menuLateral");
 console.log($divMenuLateral);
 //const $btnMenuOpen = document.querySelector('.btnMenu_open');
 
-$divMenuLateral.addEventListener('click',mostrarMenu());
+$divMenuLateral.addEventListener("click", mostrarMenu());
 
 function mostrarMenu(){
   //  $menu.classList.toggle("menu_open");
-  console.log("oi");
+  console.log("oia");
 }
 /*$btnMenuOpen.addEventListener('click',function(){
     $menu.classList.add('menu_open');
