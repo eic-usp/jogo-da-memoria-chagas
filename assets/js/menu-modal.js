11a01b828f4f8139ee6ad3f1b7a168680838a1ce
Annahlu <analuizamartinscesario@gmail.com>
2021-06-13 22:16:21 -0300

    Correções - testes

--- assets/js/menu-modal.js
+++ assets/js/menu-modal.js
@@ -1,18 +1,31 @@
-
-
 var BtnMenuModal = document.querySelector("#botao-menu-modal");
 BtnMenuModal.addEventListener("click",ShowMenuModal,false);
 
+var FadeMenuModal = document.querySelector(".fade-menu-mobile");
+FadeMenuModal.addEventListener("click",HideMenuModal,false);
+
 var menuModal = document.querySelector("#menu-modal");
 
-var BtnVoltar = document.querySelector("#botao-voltar");
-BtnVoltar.addEventListener("click",HideMenuModal,false);
+var TouchSurface = document.querySelector("#touch-surface");
 
 function ShowMenuModal(){
 	menuModal.style.zIndex = 5;
-	console.log("chamou");
-}
-function HideMenuModal(){
-	menuModal.style.zIndex = -2;
+	FadeMenuModal.style.zIndex = 5;
+	TouchSurface.style.zIndex = 5;
 }
 
+function HideMenuModal(){
+	menuModal.style.zIndex = -2;
+	FadeMenuModal.style.zIndex = -2;
+	TouchSurface.style.zIndex = -2;
+}
+
+$(function(){
+	// Bind the swipeleftHandler callback function to the swipe event on div.box
+	$("div.touch-surface").on("swipeleft", SwipeLeftFunc);
+   
+	// Callback function references the event target and adds the 'swipeleft' class to it
+	function SwipeLeftFunc(event){
+	  $(event.target).addClass("swipeleft");
+	}
+  });
