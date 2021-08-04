dfc2ee6d6c3f3afea8f8f802ba39c729320a9aea
Annahlu <analuizamartinscesario@gmail.com>
2021-08-04 18:49:11 -0300

    Aviso de cookies - não finalizado

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -1,39 +1,24 @@
+/*Se o cookie nao existe  mostra e espera a pessoa clicar. Quando a pessoa clica cria  */
+//Chamando a função
+cookieConsent();
+/*Não estou conseguindo setar o cookie como eu gostaria - perguntar isaque estratégia */
+function cookieConsent() {
+   // if (!getCookie("CookieWarning")) {
+        $(".toast").toast("show");
+        var btnCookie = document.getElementById("cookie-warning");
+        btnCookie.addEventListener("click",Accept());
+        console.log("teste cookie" + test)
+    //}else{
+      //  $(".toast").toast("hide");
+   // }
+}
 
-$(document).ready(function(){
-  $('.toast').toast('show');
-});
+function Accept(){
+    document.cookie = "CookieWarning = 1";
+    setTimeout(function (){
+        $(".toast").toast("hide");
+        }, 1000);
+}
 
 
-/*var BtnOkCookie = document.getElementById("btn-ok-cookies");
-BtnOkCookie.addEventListener("click",OkCookie, false);
 
-Cookies.set('showed', points);
-
-
-if(){
-    HideCookieWarning();
-} else{
-    OkCookie;
-}*/
-
-/*function OkCookie(){
-    /* função para mostrar aviso de cookies */
-  /*  console.log("entrou no ok cookie - primeira vez q rodou ");
-    showed = 1;
-    /* -----------------PROBLEMA ------------------- 
-    1 - depois daqui continua mostrando - ver porque o fundo não está sendo mostrado e porque o conteud continua sendo mostrado
-    2 - continua chamando a função, tentar passar algum valor por parâmetro para diferencias as chamadas */
-   /* sessionStorage.setItem('showed',showed);
-    HideCookieWarning(showed);
-    }
-/*Função para esconder os cookies */
-/*function HideCookieWarning(showed){
-    sessionStorage.setItem('showed',showed);
-   // console.log("entrou no hide 2 - vamo ve se foi essa palhaçada" + showed);
-    var componentsList = document.getElementsByClassName("text-cookies");
-    for(i=0;i<componentsList.length;i++){
-        console.log("componente" + i);
-        var componentsList = document.getElementsByClassName("text-cookies");
-        componentsList[i].classList.add("hide-cookies");
-    }
-}*/
