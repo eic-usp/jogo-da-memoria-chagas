b3cbad973af7be618cff4d8652b94dc3eaa79975
isaias1717 <icoliveira@usp.br>
2022-02-15 15:28:39 -0300

    Alguma modificações de ajuste

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,23 @@
 (function() {
+    const key = 0;
+    const user_email = " ";
+    const jogo = "Chagas";
 
-  
+
+    const nome = "";
+    const pontos = 0;
+    const nivel = 0;
+    const fase = 0;
+   
+   var data = {
+     nome: nome,
+     Pontos: pontos,
+     nivel: nivel,
+     fase: fase,
+     jogo: jogo
+   }
+
+
     const firebaseConfig = {
       apiKey: "AIzaSyAPYA5Aa8T9JtHgHFQLeDkg2nbGZRkruA4",
       authDomain: "jogomemoria-11e21.firebaseapp.com",
@@ -18,9 +35,9 @@
     //const app = initializeApp(firebaseConfig);
     
     firebase.initializeApp(firebaseConfig)
+
     //var database = firebase.database()
     //var dbRefUsers = database.ref('users')
-    
     
     const cadForm = document.getElementById('cadForm-form');
     const loguinForm = document.getElementById('loguinForm');
@@ -29,17 +46,16 @@
     const loguinGoogle = document.getElementById('loguin-Google');
 
     const emailreset = document.getElementById('email-reset')
-
-    const salvar = document.getElementById('salvar')
-
+    
     const consulta = document.getElementById('consulta')
+    const atualizacao = document.getElementById('atualizacao')
 
    firebase.auth().onAuthStateChanged((user) => {
 
     if(user){
-        //cadForm-form.reset();
-      console.log("logado " + user.email)
-
+        console.log("logado " + user.email)
+        verificarKey()
+        //código para continuar jogando de onde parou aqui
         //document.getElementById(cadForm).innerHTML = 
     } else{
 
@@ -64,9 +80,72 @@
         console.log("deslogado");
         //window.location="login.html#";
     }
-    
-
     })
+
+     
+  function verificarKey(){
+
+ 
+    // pegar usuario de email
+    const email_user_ = firebase.auth().currentUser.email
+    let user_ = email_user_.split("@")
+    this.user_email = user_[0]
+    console.log("User " + this.user_email) 
+  
+    var data = {
+      nome: user_[0],
+      Pontos: 0,
+      nivel: 0,
+      fase: 0,
+      jogo: jogo
+    }
+     
+      dbRefUsers.child(firebase.auth().currentUser.uid).child(jogo).once('value').then((snapshot) =>
+      {
+        if (snapshot.exists()) {
+          // se usuario já esta cadastrado
+          console.log("Jogo de chagas Cadastrado");
+  
+          dbRefUsers.child(firebase.auth().currentUser.uid).child(jogo).once("child_added", (snap) => {
+            // dados de pontuação e niveis e fase
+            console.log(snap.val())
+            // chave do usuario         
+            this.key = snap.key
+                     
+          });
+       
+        } else {
+          console.log("Jogo de chagas não cadastrado");
+          // criar cadastro
+          dbRefUsers.child(firebase.auth().currentUser.uid).child(jogo).push(data).then(function () {
+            console.log(data.nome + "Cadastrado")          
+          });
+  
+          dbRefUsers.child(firebase.auth().currentUser.uid).child(jogo).once("child_added", (snap) => {
+            // pegar a chave do usuario
+            this.key = snap.key
+                     
+          });
+        }
+      }).catch((error) => {
+        console.error(error);
+      });      
+      
+    }
+    // Depois
+    function GerarRank(){
+      console.log("------------------------")
+      
+      dbRefUsers.orderByChild('Pontos')
+      .on("child_added", (snap) => {
+        console.log(snap.val());
+        //console.log(snap.key)
+       // this.key = snap.key
+        
+        
+      });
+      
+    }
 
     cadForm.onsubmit = event => {
         event.preventDefault();
@@ -133,46 +212,52 @@
     }
 
     
-  /*  logoutActionModal.onclick = event => {
+   logoutActionModal.onclick = event => {
         firebase.auth().signOut();
         console.log("Sair -------------------");
         window.location="index.html";
         
     }
-    
-    salvar.onclick = event => {
-        
-        if(firebase.auth().currentUser != null){
-            var data = {
-              nome: firebase.auth().currentUser.email,
-              Pontos: document.getElementById('final-score').innerHTML,
-              jogo: "Doença de Chagas"
-            }
-            console.log(firebase.auth().currentUser.uid + '" adicionada com sucesso')
-            
-            
-            dbRefUsers.child(firebase.auth().currentUser.uid).push(data).then(function () {
-              console.log(data.nome + " Salvo")
-            })
-          } else {
-              console.log('Erro')
-          }      
-    }
 
     consulta.onclick = event => {
-
+        
         console.log('Consulta')
-        // quando tiver alteração
-        dbRefUsers.child(firebase.auth().currentUser.uid).on('value', function (dataSnapshot) {
-
-            console.log( dataSnapshot.numChildren() + " teste")
-
-            dataSnapshot.forEach((item, index)=>{
-                console.log(index, item)
-            })
+                
+        console.log(" ------- ")
+        // Pegar informações do usuario; nivel, fase
+        dbRefUsers.child(firebase.auth().currentUser.uid).child(jogo).on("child_added", (snap) => {
+            console.log(snap.val());
             
-        })
+          });       
+      
     }
-    */
-    
+
+    atualizacao.onclick = event => {
+      
+      console.log('Atualização')
+      // chave do usuario no jogo
+      console.log(this.key)
+      
+      const pontos = 250;
+      const nivel = 1;
+      const fase = 1;
+      const jogo = "Chagas";
+
+      console.log(" teste " + this.user_email)
+      var data1 = {
+        nome: this.user_email,
+        Pontos: pontos,
+        nivel: nivel,
+        fase: fase,
+        jogo: jogo
+      }
+
+      dbRefUsers.child(firebase.auth().currentUser.uid).child(jogo).child(this.key).set(data1).then(function (){
+        console.log("Certo")
+      }).catch(function (error) {
+        console.log(' Erro', error)
+      })
+
+  }
+
 })();
